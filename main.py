da4fafcfcdef9f8d68ffd8783ea54dec0feef47c
notajunster@gmail.com <notajunster@gmail.com>
2021-10-02 22:19:00 -0400

    random

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
+import random
 
 running = True
 
 ballPOSX = 500
 ballPOSY = 500
-ballSpeed = 100
-
+ballSpeed = 0.1
 ballM = 2
-ballDir2L = True;
+
+timeS = 0
+timeE = 0
 
 botPOSX = 790;
 botPOSY = 10;
@@ -26,19 +28,44 @@ while running:
 
     # Update
 
+    timeS = timeE
+    timeE = pygame.time.get_ticks()
+    timeDelta = timeE - timeS
+
+    dDelta = timeDelta * ballSpeed
+    tmpballPOSX = ballPOSX + dDelta
+    tmpballPOSY = ballPOSY + dDelta * ballM
+
+    if tmpballPOSX > 795 or tmpballPOSX < 5:
+        ballM = -1/ballM
+        ballSpeed = -ballSpeed
+
+    elif tmpballPOSY > 595 or tmpballPOSY < 5:
+        ballM = -1/ballM
+
+    else:
+        ballPOSX = tmpballPOSX
+        ballPOSY = tmpballPOSY
+
+
+    ballSpeed = (ballSpeed * (ballSpeed > 1.5)) + (ballSpeed * (random.randint(9999,10003) / 10000) * (ballSpeed < 1.5))
+    ballM += (random.randint(-102,100) / 100000)
+
+    print(ballM, "\n")
+    print(ballSpeed, "\n")
 
     # Render
 
-    screen.fill((0, 0, 0))
 
     pygame.draw.circle(screen, (255, 255, 255), (ballPOSX, ballPOSY), 5)
 
-    for i in range(0, 600, 15):
+    # for i in range(0, 600, 15):
 
-        pygame.draw.rect(screen, (255, 255, 255), (399, i, 2, 10))
+        # pygame.draw.rect(screen, (255, 255, 255), (399, i, 2, 10))
 
-    pygame.draw.rect(screen, (255, 255, 255), (botPOSX, botPOSY, 5, 75))
+    # pygame.draw.rect(screen, (255, 255, 255), (botPOSX, botPOSY, 5, 75))
 
     pygame.display.update()
+
 
 pygame.quit()
